486455826fe79e08d027ab4c749a1f3e080674f3
Chris David <cdavid@macbook.local>
2016-10-09 14:18:38 -0700

    Set up the quiz questions in an array of dictionaries

---
+++ TrueFalseStarter/QuizModel.swift
@@ -0,0 +1,108 @@
+//
+//  QuizModel.swift
+//  TrueFalseStarter
+//
+//  Created by Chris David on 10/7/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct QuizModel {
+
+    let quiz: [[String: String]] = [
+        [
+            "question": "Who is the founder of Choy Li Fut?",
+            "1": "Bruce Lee",
+            "2": "Chan Heung",
+            "3": "Jet Li",
+            "4": "Jackin Chan",
+            "answer": "2"
+        ],
+        
+        [
+            "question": "From what area of China is Choy Li Fut derived?",
+            "1": "North",
+            "2": "South",
+            "3": "Both",
+            "4": "Neither",
+            "answer": "3"
+        ],
+        
+        [
+            "question": "Who is the found of White Dragon Martial Arts?",
+            "1": "John Doe",
+            "2": "James Brown",
+            "3": "Nathan Fisher",
+            "4": "Barrack Obama",
+            "answer": "3"
+        ],
+        
+        [
+            "question": "Who is the grand master of White Dragon Martial Arts?",
+            "1": "Brandon Lee",
+            "2": "Tony Jaa",
+            "3": "Doc Fai Wong",
+            "4": "Jean-Claude Van Damme",
+            "answer": "3"
+        ],
+        
+        [
+            "question": "What is a tsop choi?",
+            "1": "Kick",
+            "2": "Palm Strike",
+            "3": "Spear Hand",
+            "4": "Leopard Fist",
+            "answer": "4"
+        ],
+        
+        [
+            "question": "Translate Siu Sup Ji Kuen",
+            "1": "Small Hand Form",
+            "2": "Small Cross Pattern Hand Form",
+            "3": "Big Hand Form",
+            "4": "Big Cross Chain ",
+            "answer": "2"
+        ],
+        
+        [
+            "question": "Translate Siu Mui Fa Kuen",
+            "1": "Pretty Flower Form",
+            "2": "Small Plum Blossom Hand Form",
+            "3": "Cactus Hand Form",
+            "4": "Small Tree Form",
+            "answer": "2"
+        ],
+        
+        [
+            "question": "What is martial spirit?",
+            "1": "Resolve",
+            "2": "Weakness",
+            "3": "Brute Strength",
+            "4": "Skil",
+            "answer": "1"
+        ],
+        
+        [
+            "question": "Apart of what organization is White Dragon?",
+            "1": "Plub Blossom Federation",
+            "2": "International Kung Fu Federation",
+            "3": "Federation of Martials Arts",
+            "4": "Kung Fu Club",
+            "answer": "1"
+        ],
+        
+        [
+            "question": "What is a fu jow?",
+            "1": "Uppercut",
+            "2": "Low Kick",
+            "3": "Dragon Claw",
+            "4": "Tiger Claw",
+            "answer": "4"
+        ]
+
+    ]
+
+    
+    
+}
